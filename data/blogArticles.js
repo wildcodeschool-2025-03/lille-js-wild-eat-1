10a9670d5c76ae76a899b5378c9ee7740f911ea9
Jordan-182 <jordan.p62@outlook.fr>
2025-03-24 17:21:55 +0100

    init blog branch

---
+++ data/blogArticles.js
@@ -0,0 +1,9 @@
+const blogArticles = [
+  {
+    id: 0,
+    title: "titre de l'article",
+    date: "date de l'article",
+    img: "source de l'image",
+    content: "contenu de l'article"
+  }
+];
